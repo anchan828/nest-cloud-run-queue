858e4c0e57071436a7199e8e7468ae5a807fa20a
anchan828 <anchan828@gmail.com>
2020-01-15 02:04:15 +0900

    fix: export CloudRunPubSubService

--- packages/publisher/src/index.ts
+++ packages/publisher/src/index.ts
@@ -1,3 +1,4 @@
 export * from "./constants";
 export * from "./interfaces";
 export { CloudRunPubSubPublisherModule } from "./publish.module";
+export { CloudRunPubSubService } from "./publish.service";
