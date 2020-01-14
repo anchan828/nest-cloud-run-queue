7fe76198b30ce2efa20ae14b15dfc3adc87ffaf9
anchan828 <anchan828@gmail.com>
2020-01-15 02:14:32 +0900

    chore: update exports

--- packages/publisher/src/index.ts
+++ packages/publisher/src/index.ts
@@ -1,4 +1,8 @@
-export * from "./constants";
-export * from "./interfaces";
+export { CLOUD_RUN_PUBSUB, CLOUD_RUN_PUBSUB_PUBLISHER_MODULE_OPTIONS } from "./constants";
+export {
+  CloudRunPubSubPublisherModuleAsyncOptions,
+  CloudRunPubSubPublisherModuleOptions,
+  CloudRunPubSubPublisherModuleOptionsFactory,
+} from "./interfaces";
 export { CloudRunPubSubPublisherModule } from "./publish.module";
 export { CloudRunPubSubService } from "./publish.service";
--- packages/worker/src/index.ts
+++ packages/worker/src/index.ts
@@ -1,4 +1,9 @@
-export * from "./constants";
-export * from "./decorators";
-export * from "./interfaces";
+export { CloudRunPubSubWorker, CloudRunPubSubWorkerProcess } from "./decorators";
+export {
+  CloudRunPubSubWorkerModuleAsyncOptions,
+  CloudRunPubSubWorkerModuleOptions,
+  CloudRunPubSubWorkerModuleOptionsFactory,
+  CloudRunPubSubWorkerProcessor,
+} from "./interfaces";
+export { PubSubMessageDto, PubSubRootDto } from "./message.dto";
 export { CloudRunPubSubWorkerModule } from "./worker.module";
