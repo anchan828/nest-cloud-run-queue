e7e34961346ec4cbab8131334c826aaf0abf1290
Keigo Ando <anchan828@gmail.com>
2022-05-26 18:37:56 +0900

    test: add tests

--- packages/worker/src/worker.module.spec.ts
+++ packages/worker/src/worker.module.spec.ts
@@ -1,10 +1,12 @@
-import { Logger, RequestMethod } from "@nestjs/common";
+import { Body, Controller, Logger, Post, RequestMethod } from "@nestjs/common";
 import { MetadataScanner } from "@nestjs/core/metadata-scanner";
 import { Test } from "@nestjs/testing";
 import { QUEUE_WORKER_MODULE_OPTIONS } from "./constants";
 import { QueueWorkerExplorerService } from "./explorer.service";
-import { QueueWorkerModuleOptions } from "./interfaces";
+import { QueueWorkerModuleOptions, QueueWorkerReceivedMessage } from "./interfaces";
 import { QueueWorkerModule } from "./worker.module";
+import { QueueWorkerService } from "./worker.service";
+
 describe("QueueWorkerModule", () => {
   it("should compile with register", async () => {
     const app = await Test.createTestingModule({
@@ -15,6 +17,29 @@ describe("QueueWorkerModule", () => {
     expect(app.get<QueueWorkerExplorerService>(QueueWorkerExplorerService)).toBeDefined();
     expect(app.get<Logger>(Logger)).toBeDefined();
     expect(app.get<QueueWorkerModuleOptions>(QUEUE_WORKER_MODULE_OPTIONS)).toBeDefined();
+  });
+
+  it("should use custom controller", async () => {
+    @Controller("/worker")
+    class WorkerController {
+      constructor(private readonly service: QueueWorkerService) {}
+
+      @Post()
+      public async execute(@Body() body: QueueWorkerReceivedMessage): Promise<void> {
+        await this.service.execute({ ...body.message });
+      }
+    }
+
+    const app = await Test.createTestingModule({
+      controllers: [WorkerController],
+      imports: [QueueWorkerModule.register({ workerController: null })],
+    }).compile();
+    expect(app).toBeDefined();
+    expect(app.get<MetadataScanner>(MetadataScanner)).toBeDefined();
+    expect(app.get<QueueWorkerExplorerService>(QueueWorkerExplorerService)).toBeDefined();
+    expect(app.get<Logger>(Logger)).toBeDefined();
+    expect(app.get<QueueWorkerModuleOptions>(QUEUE_WORKER_MODULE_OPTIONS)).toBeDefined();
+    expect(app.get<WorkerController>(WorkerController)).toBeDefined();
   });
 
   it("should compile with registerAsync", async () => {
@@ -30,10 +55,32 @@ describe("QueueWorkerModule", () => {
       ],
     }).compile();
     expect(app).toBeDefined();
-
     expect(app.get<MetadataScanner>(MetadataScanner)).toBeDefined();
     expect(app.get<QueueWorkerExplorerService>(QueueWorkerExplorerService)).toBeDefined();
     expect(app.get<Logger>(Logger)).toBeDefined();
     expect(app.get<QueueWorkerModuleOptions>(QUEUE_WORKER_MODULE_OPTIONS)).toBeDefined();
+  });
+
+  it("should use custom controller", async () => {
+    @Controller("/worker")
+    class WorkerController {
+      constructor(private readonly service: QueueWorkerService) {}
+
+      @Post()
+      public async execute(@Body() body: QueueWorkerReceivedMessage): Promise<void> {
+        await this.service.execute({ ...body.message });
+      }
+    }
+
+    const app = await Test.createTestingModule({
+      controllers: [WorkerController],
+      imports: [QueueWorkerModule.register({ workerController: null })],
+    }).compile();
+    expect(app).toBeDefined();
+    expect(app.get<MetadataScanner>(MetadataScanner)).toBeDefined();
+    expect(app.get<QueueWorkerExplorerService>(QueueWorkerExplorerService)).toBeDefined();
+    expect(app.get<Logger>(Logger)).toBeDefined();
+    expect(app.get<QueueWorkerModuleOptions>(QUEUE_WORKER_MODULE_OPTIONS)).toBeDefined();
+    expect(app.get<WorkerController>(WorkerController)).toBeDefined();
   });
 });
